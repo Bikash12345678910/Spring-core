3944ffc3e4c0a3e09e5abe884ab4df2502b3b2cc
Bikash Sau <bikashkumarsau0012@gmail.com>
2026-03-28 00:40:45 +0530

    Add files via upload

---
+++ removing_xml/src/main/java/com/ty/Person.java
@@ -0,0 +1,11 @@
+package com.ty;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+    public void eating(){
+        System.out.println("eating");
+    }
+
+}
