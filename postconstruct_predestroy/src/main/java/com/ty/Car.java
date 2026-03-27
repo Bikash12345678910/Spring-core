63b095dd2f06e72939569c334197b8a857d57ba5
Bikash Sau <bikashkumarsau0012@gmail.com>
2026-03-28 00:40:06 +0530

    Add files via upload

---
+++ postconstruct_predestroy/src/main/java/com/ty/Car.java
@@ -0,0 +1,13 @@
+package com.ty;
+
+public class Car {
+    public Car(){
+        System.out.println("I am a Constructor");
+    }
+    public void start(){
+        System.out.println("I am init method");
+    }
+    public void stop(){
+        System.out.println("I am  destroy method");
+    }
+}
